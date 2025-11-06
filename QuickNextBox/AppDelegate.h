983d496cb8c8c14e2081b9119204f0072d1b9aeb
liguancheng <2960853985@qq.com>
2025-11-06 23:59:16 +0800

    Initial Commit

---
+++ QuickNextBox/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  QuickNextBox
+//
+//  Created by lgc on 2025/11/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ QuickNextBox/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  QuickNextBox
+//
+//  Created by lgc on 2025/11/6.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
